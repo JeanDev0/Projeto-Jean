bb06c79bca8c0cb0de41fdfb552ae908a60ee04b
Jean <jean.mpfernandes@gmail.com>
2026-03-20 13:43:06 -0300

    Passando do fazendo para feito

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,32 @@
 let botao = document.getElementById("btn-fazendo")
 let input = document.getElementById("input-fazendo")
 let lista = document.getElementById("lista-fazendo")
+let concluido = document.getElementById("lista-feito")
+
+botao.addEventListener("click",() =>{
+    let texto = input.value;
+    if (!texto) return;
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        <input class="finalizado" type="checkbox">
+        ${texto}
+    `;
+
+    const checkbox = li.querySelector(".finalizado");
+
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            concluido.appendChild(li);
+        } else {
+            lista.appendChild(li);
+        }
+
+    })
+
+    lista.appendChild(li);
+    input.value = "";
+
+})
+
